22e43ccdd51e62f2b5be593e31083ad0baf5ba07
juan.calderon <juan.calderon@YVERDON.cimav.cimav.edu.mx>
2015-05-14 15:29:22 -0600

    Correccion

--- src/main/java/cimav/restrh/services/EmpleadoFacadeREST.java
+++ src/main/java/cimav/restrh/services/EmpleadoFacadeREST.java
@@ -58,9 +58,11 @@ public class EmpleadoFacadeREST extends AbstractFacade<Empleado> {
         this.setName(entity);
         super.insert(entity); // <-- regresa con el Id nuevo
         
-        Query query = getEntityManager().createQuery("SELECT NEW cimav.restrh.entities.Empleado(e.id, e.code, e.name, e.cuentaCimav) FROM Empleado AS e WHERE e.id = :p_id", Empleado.class);
+        Query query = getEntityManager().createQuery("SELECT NEW cimav.restrh.entities.Empleado(e.id, e.code) FROM Empleado AS e WHERE e.id = :p_id", Empleado.class);
         query.setParameter("p_id", entity.getId());
-        entity = (Empleado) query.getSingleResult();
+        Empleado empNuevo = (Empleado) query.getSingleResult();
+        
+        entity.setCode(empNuevo.getCode());
         
         return entity; 
     }
